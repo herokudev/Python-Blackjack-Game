94d8771fd498629377e316c22a34a2152af157b2
herokudev <ao43085@gmail.com>
2022-01-11 18:24:30 -0600

    Adding the Player class
    Adding the Player class

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from random import shuffle
+from typing import final
 
 
 def createDeck():
@@ -20,3 +21,39 @@ cardDeck = createDeck()
 shuffle = shuffle(cardDeck)
 
 print(cardDeck)
+
+
+class Player:
+    def __init__(self, hand=[], money=100):
+        self.hand = hand
+        self.score = self.setScore()
+        self.money = money
+
+    def __str__(self):  # print(Player)
+        currentHand = ""  # self.hand = ["A", "10"]
+
+        for card in self.hand:
+            currentHand += str(card) + " "  # "A 10"
+
+        finalStatus = currentHand + "score: " + \
+            str(self.score)  # "A 10 2 4 --> score=17"
+        return finalStatus
+
+    def setScore(self):
+        self.score = 0
+        faceCardsDict = {"A": 11, "J": 10, "Q": 10, "K": 10, "2": 2,
+                         "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10}
+
+        aceCounter = 0
+        for card in self.hand:
+            self.score += faceCardsDict[card]
+            if card == "A":
+                aceCounter += 1
+            if self.score > 21 and aceCounter != 0:
+                self.score -= 10
+                aceCounter -= 1
+        return self.score
+
+
+Player1 = Player(["8", "A", "5"])
+print(Player1)
